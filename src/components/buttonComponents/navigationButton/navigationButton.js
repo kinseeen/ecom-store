5b00fa9a8672a2baf6d3a3e9a79b564c6d02c8a2
kinseeen <kine.jakobsen@hotmail.no>
2025-05-12 11:55:28 +0200

    add new functionality to nav button
    Added if else so that button navigates to homepage aswell.

--- src/components/buttonComponents/navigationButton/navigationButton.js
+++ src/components/buttonComponents/navigationButton/navigationButton.js
@@ -2,11 +2,19 @@ import React from "react";
 import "./navigationButton.css";
 import { useNavigate } from "react-router-dom";
 
-function NavigationButton({ buttonText }) {
+function NavigationButton({ buttonText, to }) {
   const navigate = useNavigate();
 
+  const handleClick = () => {
+    if (to) {
+      navigate(to);
+    } else {
+      navigate(-1);
+    }
+  };
+
   return (
-    <button className="navigationButton" onClick={() => navigate(-1)}>
+    <button className="navigationButton" onClick={handleClick}>
       {buttonText}
     </button>
   );
--- src/components/pages/checkOutSuccessPage/checkOutSuccessPage.js
+++ src/components/pages/checkOutSuccessPage/checkOutSuccessPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../checkOutSuccessPage/checkOutSuccessPage.css";
+import NavigationButton from "../../buttonComponents/navigationButton/navigationButton";
 
 const CheckoutSuccessPage = () => {
   return (
     <div className="checkOutPage">
       <h1> Thank you for your order. </h1>
       <p> Your order has been placed! </p>
+      <NavigationButton buttonText="Return to homepage" to="/" />
     </div>
   );
 };
